976ba8b084a765b52f8118af0c0bc4b83ad1dd5a
Luke Storry <luke.storry@newtoneurope.com>
2022-01-18 17:53:50 +0000

    refactor & add docs

--- optimisation.py
+++ optimisation.py
@@ -11,6 +11,8 @@ seed(20)
 
 
 class Action:
+    """A Single action that our Agent can choose to apply"""
+
     def __init__(self, step: int, recipient: PurchaseOrder, parameter_name: str) -> None:
         self.step = step
         self.recipient = recipient
@@ -18,6 +20,7 @@ class Action:
         self.reward: float = 0
 
     def apply(self):
+        """Alter the recipient by applying a step, limiting at 0"""
         old_value = getattr(self.recipient, self.parameter_name)
         new_value = max(0, old_value + self.step)
         setattr(self.recipient, self.parameter_name, new_value)
@@ -27,17 +30,20 @@ class Action:
 
 
 class Agent:
-    def __init__(self, purchase_orders: List[PurchaseOrder], eps: int) -> None:
+    """An Epsilon-Greedy Agent with a collection of Actions based on given PurchaseOrders"""
+
+    def __init__(self, purchase_orders: List[PurchaseOrder], eps: int):
         self.eps = eps
         self.actions = [Action(step, purchase_order, parameter_name)
                         for purchase_order in purchase_orders
-                        for parameter_name in ("date", "amount")
+                        for parameter_name in ("day", "amount")
                         for step in (-1, +1)
                         ]
         self.chosen_action: Action = None
         self.rewards: List[int] = []
 
     def choose_action(self) -> Action:
+        """Either Explore a random action, or Exploit the action with best reward, depending on EPS"""
         if random() < self.eps:
             self.chosen_action = choice(self.actions)
         else:
@@ -46,10 +52,12 @@ class Agent:
         return self.chosen_action
 
     def apply_reward(self, reward: int):
+        """Update the latest Action with the given reward, and keep track of the reward internally"""
         self.rewards.append(reward)
         self.chosen_action.reward = reward
 
     def plot(self):
+        """Plots the Reward over timr throughout the optimisation"""
         plt.plot(self.rewards)
         plt.title('Reward over time')
         plt.xlabel('Iteration')
@@ -57,18 +65,21 @@ class Agent:
         plt.show()
 
 
-def run_optimiser(eps = 0.2):
+def run_optimiser(eps=0.2, iterations=5000, target=20):
+    """Repeatedly use the agent to find optimum input to the simulation."""
     purchase_orders = [PurchaseOrder(200, 5) for _ in range(10)]
     agent = Agent(purchase_orders, eps)
-    for _ in tqdm(range(10000)):
+    for _ in tqdm(range(iterations)):
         agent.choose_action().apply()
         simulation = Simulation(purchase_orders)
-        agent.apply_reward(1000 - simulation.calculate_mean_squared_error() - simulation.calculate_sum_under_target())
-    # simulation.plot()
+        simulation.run()
+        agent.apply_reward(1000 - simulation.calculate_mean_squared_error(target) - simulation.calculate_sum_under_target(target))
+    simulation.plot(target)
     return agent
 
+
 if __name__ == "__main__":
-    for eps in (0, 0.1, 0.2, 0.3, 0.4, 0.5):
+    for eps in [0.2]:  # (0, 0.1, 0.2, 0.3, 0.4, 0.5):
         agent = run_optimiser(eps)
         plt.plot(agent.rewards, label=eps)
 
--- simulation.py
+++ simulation.py
@@ -5,64 +5,77 @@ import simpy
 
 
 class PurchaseOrder:
-    def __init__(self, date: int, amount: int) -> None:
-        self.date = date
+    """An object to store Orders of an amount of Items on a certain day"""
+
+    def __init__(self, day: int, amount: int) -> None:
+        self.day = day
         self.amount = amount
 
     def __repr__(self) -> str:
-        return f"<PurchaseOrder for {self.amount} at {self.date}>"
+        return f"<PurchaseOrder for {self.amount} at {self.day}>"
 
 
 class Item:
+    """A single degradable inventory Item"""
+
     def __init__(self, age: int) -> None:
         self.age = age
-
-    def is_available(self):
-        return self.age < self.limit
 
     def __repr__(self) -> str:
         return f"<Item with age of {self.age}>"
 
 
 class Simulation:
-    def __init__(self, purchase_orders=List[PurchaseOrder], run_length: int = 365) -> None:
-        initial_ages = [0, 0, 0, 1, 1, 5, 5, 10, 10, 50, 50, 80, 80]
-        self.age_limit = 100
+    """A Simulation of inventory over time"""
 
+    def __init__(self,
+                 purchase_orders=List[PurchaseOrder],
+                 initial_ages=[0, 0, 0, 1, 1, 5, 5, 10, 10, 50, 50, 80, 80],
+                 age_limit=100):
+
+        self.age_limit = age_limit
         self.inventory = [Item(age) for age in initial_ages]
         self.availabilities: dict[int, int] = {}
+        self.purchase_orders = sorted(purchase_orders, key=lambda p: p.day)
 
+    def run(self, run_length: int = 365):
         self.environment = simpy.Environment()
         self.environment.process(self.daily_age_increment())
-        self.environment.process(self.handle_purchases(purchase_orders))
+        self.environment.process(self.handle_purchases())
         self.environment.process(self.store_availabilities())
         self.environment.run(run_length)
 
     def daily_age_increment(self):
+        """Simpy Generator to increase the age of all Items each day"""
         while True:
             for item in self.inventory:
                 item.age += 1  # + random()
             yield self.environment.timeout(1)
 
     def store_availabilities(self):
+        """Simpy Generator to monitor the total availability each day"""
         while True:
             available = sum(1 for item in self.inventory if item.age < self.age_limit)
             self.availabilities[self.environment.now] = available
             yield self.environment.timeout(1)
 
-    def handle_purchases(self, purchase_orders: List[PurchaseOrder]):
-        for purchase in sorted(purchase_orders, key=lambda p: p.date):
-            yield self.environment.timeout(purchase.date - self.environment.now)
+    def handle_purchases(self):
+        """Simpy Generator to create Items from PurchaseOrders and add them to the inventory"""
+        for purchase in self.purchase_orders:
+            yield self.environment.timeout(purchase.day - self.environment.now)
             self.inventory.extend(([Item(0) for _ in range(purchase.amount)]))
 
     def calculate_mean_squared_error(self, target=30) -> int:
+        """Calculates the MSD of the availabilities compared to a target, Post-Simulation"""
         sum_of_squared_error = sum((target - value)**2 for value in self.availabilities.values())
         return round(sum_of_squared_error/len(self.availabilities))
 
     def calculate_sum_under_target(self, target=30) -> int:
+        """Calculates the amount of under-stocking across all days, Post-Simulation"""
         return sum((target - value) for value in self.availabilities.values() if value < target)
 
     def plot(self, target: int = 30):
+        """Plots the availabilities on a line chart"""
         plt.plot(list(self.availabilities.keys()), list(self.availabilities.values()))
         plt.axhline(y=target, color='r', linestyle='--')
         plt.title('Availability over time')
@@ -73,8 +86,9 @@ class Simulation:
 
 
 if __name__ == "__main__":
-    p = [PurchaseOrder(time, 6) for time in range(50, 500, 15)]
+    p = [PurchaseOrder(time, 5) for time in range(10, 500, 15)]
     s = Simulation(p)
+    s.run()
     print(s.calculate_mean_squared_error())
     print(s.calculate_sum_under_target())
     s.plot()
